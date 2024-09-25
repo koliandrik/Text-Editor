6903801e23587e5baf554ad3b45eeb5484591dd5
koliandrik <koliandrik@gmail.com>
2024-09-24 19:16:27 -0700

    server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the client/dist directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => {
-  console.log(`Now listening on port: ${PORT}`);
-});
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
